424eb1175730d4b256f404e3520c853b5c8cdcc3
yaminchu <y.myaomyao@gmail.com>
2018-11-03 22:27:47 +0900

    makenewsportspage initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,28 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { File } from '@ionic-native/file';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer' ;
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MakeNewSportsPage } from '../pages/make-new-sports/make-new-sports';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MakeNewSportsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MakeNewSportsPage
   ],
   providers: [
     StatusBar,
