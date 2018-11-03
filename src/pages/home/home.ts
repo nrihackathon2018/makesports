0336b10ab40a22c31f3ed920a2cb125503760058
yaminchu <y.myaomyao@gmail.com>
2018-11-04 03:16:10 +0900

    画面遷移実装

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MakeNewSportsPage } from '../make-new-sports/make-new-sports';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  goToMakeNewSportsPage(){
+    this.navCtrl.push(MakeNewSportsPage);
+  }
 }
