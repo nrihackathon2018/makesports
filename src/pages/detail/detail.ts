727b2dcb028eda04e2e27bff79d5f86a78d6f196
unknown <505cadjust@gmail.com>
2018-11-04 11:14:20 +0900

    make kintone hardcording

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -28,63 +28,8 @@ export class DetailPage {
     this.sportsId = navParams.get('itemId');
     this.SportsDetails = this.kintoneProvider.getMakeSports(this.sportsId);
     console.log("this.SportsDetails",this.SportsDetails);
-    this.SportsDetails ={
-      "records": [
-        {
-          "スポーツID": {
-            "type": 'NUMBER',
-            "value": "1"
-          },
-          "スポーツ名": {
-            "type": "SINGLE_LINE_TEXT",
-            "value": [
-              "採用"
-            ]
-          },
-          "スポーツ説明": {
-            "type": "SINGLE_LINE_TEXT",
-            "value": "やってません。"
-          },
-          "画像URL": {
-            "type": "RECORD_NUMBER",
-            "value": "1"
-          },
-          "更新者": {
-            "type": "MODIFIER",
-            "value": {
-              "code": "Administrator",
-              "name": "Administrator"
-            }
-          },
-          "作成者": {
-            "type": "CREATOR",
-            "value": {
-              "code": "Administrator",
-              "name": "Administrator"
-            }
-          },
-          "$revision": {
-            "type": "__REVISION__",
-            "value": "1"
-          },
-          "更新日時": {
-            "type": "UPDATED_TIME",
-            "value": "2017-02-22T16:49:00Z"
-          },
-          "作成日時": {
-            "type": "CREATED_TIME",
-            "value": "2017-02-22T16:49:00Z"
-          },
-          "$id": {
-            "type": "__ID__",
-            "value": "1"
-          }
-        }
-      ],
-      "totalCount": null
-     }
-    // this.SportsImage = this.SportsDetails.records[0].画像URL.value;
-    this.SportsImage = "/assets/imgs/snoop_dogg.gif";
+    this.SportsImage = this.SportsDetails.records[0].画像URL.value;
+    // this.SportsImage = "/assets/imgs/snoop_dogg.gif";
     this.SportsName = this.SportsDetails.records[0].スポーツ名.value;
     this.SportsDesc = this.SportsDetails.records[0].スポーツ説明.value
     console.log("this.SportsImage",this.SportsImage);
--- src/providers/kintone/kintone.ts
+++ src/providers/kintone/kintone.ts
@@ -13,8 +13,9 @@ import kintone from 'kintone-nodejs-sdk';
 */
 @Injectable()
 export class KintoneProvider {
-
+  SportsDetails;
   constructor() {
+    
   }
 
   getMakeSports(sportsId){
@@ -29,18 +30,313 @@ export class KintoneProvider {
     let kintoneConnection = new kintone.Connection(myDomainName, kintoneAuth);
 
     let kintoneRecord = new kintone.Record(kintoneConnection);
-    let appID = 9; // target appID
+    // let appID = 9; // target appID
 
     // let recordID = sportsId;
-    let recordID = 1;
-    kintoneRecord.getRecord(appID,recordID)
-        .then((rsp) => {
-            console.log(rsp);
-            return rsp;
-        })
-        .catch((err) => {
-            console.log(err.get());
-        });
-    return;
+    // let recordID = 1;
+    if(sportsId == 1){
+      this.SportsDetails ={
+        "records": [
+          {
+            "スポーツID": {
+              "type": 'NUMBER',
+              "value": "1"
+            },
+            "スポーツ名": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": [
+                "りんごの皮むき"
+              ]
+            },
+            "スポーツ説明": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": "りんごまるごとの皮の早むき。"
+            },
+            "画像URL": {
+              "type": "RECORD_NUMBER",
+              "value": "/assets/imgs/apple.jpg"
+            },
+            "更新者": {
+              "type": "MODIFIER",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "作成者": {
+              "type": "CREATOR",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "$revision": {
+              "type": "__REVISION__",
+              "value": "1"
+            },
+            "更新日時": {
+              "type": "UPDATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "作成日時": {
+              "type": "CREATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "$id": {
+              "type": "__ID__",
+              "value": "1"
+            }
+          }
+        ],
+        "totalCount": null
+       }
+      return this.SportsDetails;
+    }
+
+    if(sportsId == 2){
+      this.SportsDetails ={
+        "records": [
+          {
+            "スポーツID": {
+              "type": 'NUMBER',
+              "value": "2"
+            },
+            "スポーツ名": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": [
+                "ベイブレード"
+              ]
+            },
+            "スポーツ説明": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": "ぶーーーーーん"
+            },
+            "画像URL": {
+              "type": "RECORD_NUMBER",
+              "value": "/assets/imgs/koma.jpg"
+            },
+            "更新者": {
+              "type": "MODIFIER",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "作成者": {
+              "type": "CREATOR",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "$revision": {
+              "type": "__REVISION__",
+              "value": "1"
+            },
+            "更新日時": {
+              "type": "UPDATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "作成日時": {
+              "type": "CREATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "$id": {
+              "type": "__ID__",
+              "value": "1"
+            }
+          }
+        ],
+        "totalCount": null
+       }
+      return this.SportsDetails;
+    }
+
+    if(sportsId == 3){
+      this.SportsDetails ={
+        "records": [
+          {
+            "スポーツID": {
+              "type": 'NUMBER',
+              "value": "3"
+            },
+            "スポーツ名": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": [
+                "トランプタワー組立て"
+              ]
+            },
+            "スポーツ説明": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": "Great America"
+            },
+            "画像URL": {
+              "type": "RECORD_NUMBER",
+              "value": "/assets/imgs/tramp.jpg"
+            },
+            "更新者": {
+              "type": "MODIFIER",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "作成者": {
+              "type": "CREATOR",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "$revision": {
+              "type": "__REVISION__",
+              "value": "1"
+            },
+            "更新日時": {
+              "type": "UPDATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "作成日時": {
+              "type": "CREATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "$id": {
+              "type": "__ID__",
+              "value": "1"
+            }
+          }
+        ],
+        "totalCount": null
+       }
+      return this.SportsDetails;
+    }
+
+    if(sportsId == 4){
+      this.SportsDetails ={
+        "records": [
+          {
+            "スポーツID": {
+              "type": 'NUMBER',
+              "value": "4"
+            },
+            "スポーツ名": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": [
+                "けんだま"
+              ]
+            },
+            "スポーツ説明": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": "穴♂にさす"
+            },
+            "画像URL": {
+              "type": "RECORD_NUMBER",
+              "value": "/assets/imgs/kenball.jpg"
+            },
+            "更新者": {
+              "type": "MODIFIER",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "作成者": {
+              "type": "CREATOR",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "$revision": {
+              "type": "__REVISION__",
+              "value": "1"
+            },
+            "更新日時": {
+              "type": "UPDATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "作成日時": {
+              "type": "CREATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "$id": {
+              "type": "__ID__",
+              "value": "1"
+            }
+          }
+        ],
+        "totalCount": null
+       }
+      return this.SportsDetails;
+    }
+
+    if(sportsId == 5){
+      this.SportsDetails ={
+        "records": [
+          {
+            "スポーツID": {
+              "type": 'NUMBER',
+              "value": "5"
+            },
+            "スポーツ名": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": [
+                "ストップウォッチ止め"
+              ]
+            },
+            "スポーツ説明": {
+              "type": "SINGLE_LINE_TEXT",
+              "value": "8.6秒バズーカ"
+            },
+            "画像URL": {
+              "type": "RECORD_NUMBER",
+              "value": "/assets/imgs/stop.png"
+            },
+            "更新者": {
+              "type": "MODIFIER",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "作成者": {
+              "type": "CREATOR",
+              "value": {
+                "code": "Administrator",
+                "name": "Administrator"
+              }
+            },
+            "$revision": {
+              "type": "__REVISION__",
+              "value": "1"
+            },
+            "更新日時": {
+              "type": "UPDATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "作成日時": {
+              "type": "CREATED_TIME",
+              "value": "2017-02-22T16:49:00Z"
+            },
+            "$id": {
+              "type": "__ID__",
+              "value": "1"
+            }
+          }
+        ],
+        "totalCount": null
+       }
+      return this.SportsDetails;
+    }
+
+    // kintoneRecord.getRecord(appID,recordID)
+    //     .then((rsp) => {
+    //         console.log(rsp);
+    //         return rsp;
+    //     })
+    //     .catch((err) => {
+    //         console.log(err.get());
+    //     });
+    // return;
   }
 }
